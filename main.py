dbe4d5258ae7a8408faaa097649ebae1fd833718
JVelasco-OBFV <velasco.jp@obmontessori.edu.ph>
2026-09-15 22:14:18 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from pyscript import document, display
 def show_order(e):
     document.getElementById("output1").innerHTML = ""
     document.getElementById("output2").innerHTML = ""    
-    size = document.querySelector("input[name='size']:checked")
-    price = float(size.value)
-    pizza = document.getElementById("prod1")
+    size = document.querySelector("input[name='size']:checked") #selects input fields with the keyword 'size'
+    price = float(size.value) #retrieves the value from the radio input
+    pizza = document.getElementById("prod1") 
     drinks = document.getElementById("prod2")
-    pizza_price = float(pizza.value)
+    pizza_price = float(pizza.value) 
     drink_price = float(drinks.value)
-    subtotal = (price + pizza_price + drink_price)
-    tax = 0.12 * subtotal
-    total = float(tax) + float(subtotal)
+    subtotal = (price + pizza_price + drink_price) #adds the prices from the input and select fields to generate an initial price (without tax)
+    tax = 0.12 * subtotal #calculates the tax prices from overall items
+    total = float(tax) + float(subtotal) #calculates the final price of overall items including tax
     display('===RECEIPT===', target="output1")
     display(f'Subtotal: {subtotal}', target="output2")
     display(f'Tax: {tax}', target="output2", append=True)
